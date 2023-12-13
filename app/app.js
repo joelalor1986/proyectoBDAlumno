240d2f8ba44538d7742c7c9a5d1dccf9af76ea77
joelalor1986 <joelalor1986@gmail.com>
2023-12-12 22:00:53 -0600

    agregar buscar alumnos

--- app/app.js
+++ app/app.js
@@ -168,7 +168,14 @@ function eliminarMateria(id) {
     cargarTablaMaterias();
 }
 $("body").addEventListener("click", (e) => {
-    
+    if (e.target.id == "btnBuscarAlumno"){
+        let valorBuscar = document.querySelector("#txtBuscarAlumno").value;
+        let opcion = document.querySelector("#selectBuscar").value;
+        let resultadoBusqueda = buscarAlumnos(valorBuscar,opcion);
+        cargarTablaAlumnos2(resultadoBusqueda);
+        let modal = bootstrap.Modal.getInstance("#modalBuscarAlumno");
+        modal.hide();
+    }
     if (e.target.id == "agregarGrupo"){
         
         mostrarArr();
@@ -234,4 +241,86 @@ function mostrarArr(){
     html += `</table>`
     console.log(html);
     $("#tablasDatos").innerHTML = html;
+}
+
+let divtabla = document.querySelector("#tablasDatos");
+divtabla.innerHTML = "<p>hola mundo</p>";
+
+function buscarAlumnos(valorBuscar,opcionBusqueda){
+    let resultado = [];
+
+    if(opcionBusqueda == "nombre"){
+        controladorBD.baseDatos.tablaAlumnos.forEach( function(alumno){
+            if(alumno.nombre == valorBuscar){
+                resultado.push(alumno);
+            }
+        });
+    }
+    if(opcionBusqueda == "apellidos"){
+        controladorBD.baseDatos.tablaAlumnos.forEach(function (alumno) {
+            if (alumno.apellidos == valorBuscar) {
+                resultado.push(alumno);
+            }
+        });
+    }
+    if (opcionBusqueda == "edad") {
+        controladorBD.baseDatos.tablaAlumnos.forEach(function (alumno) {
+            if (alumno.edad == valorBuscar) {
+                resultado.push(alumno);
+            }
+        });
+    }
+    console.log(resultado);
+
+    return resultado;
+}
+function cargarTablaAlumnos2(alumnos) {
+    let tablaAlumno = alumnos;
+    let tablaContenedor = document.querySelector("#tablasDatos");
+    let tablaAlumnohtml = `<table class="table table table-striped table-hover">
+    <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    `;
+    tablaAlumno.forEach((alumno) => {
+        tablaAlumnohtml += `<tr>
+            <td>${alumno.id}</td>
+            <td>${alumno.nombre}</td>
+            <td>${alumno.apellidos}</td>
+            <td>${alumno.edad}</td>
+            <td>
+              <div class=class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                <button type="button" class="btn btn-success" title="Editar usuario"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+</svg></button>
+                <button type="button" class="btn btn-primary" onClick=agregarMateriaAlumno(${alumno.id}) title="Agregar materias" dat-bs-toggle="modal" data-bs-target="#modalAgregarMateriaAlumno"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+  <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+</svg></button>
+ <button class="btn btn-warning" title="lista de materias" onClick="cargarTablaMateriasAlumno(${alumno.id})" >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+  <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
+</svg>
+                </button>
+                <button type="button" class="btn btn-danger" onClick=eliminarAlumno(${alumno.id}) title="eliminar"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-dash" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M11 12h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m0-7a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+  <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
+</svg></button>
+               
+              </div>
+            </td>
+        </tr>`;
+        //debugger
+        //console.log(tablaAlumnohtml);
+    });
+    tablaAlumnohtml += `</tbody></table>`;
+    //console.log(tablaAlumnohtml)
+    tablaContenedor.innerHTML = tablaAlumnohtml;
 }
