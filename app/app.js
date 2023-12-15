44bf81a330c02e985bb9b7b35a510f2bca522472
joelalor1986 <joelalor1986@gmail.com>
2023-12-15 11:38:18 -0600

    agreganfo funciones del menu grupos

--- app/app.js
+++ app/app.js
@@ -7,11 +7,13 @@ if (localStorage.getItem(bd) != null) {
     controladorBD.baseDatos.keyAlumno = bdRecuperada.keyAlumnos;
     controladorBD.baseDatos.keyMateria = bdRecuperada.keyMaterias;
     controladorBD.baseDatos.keyAlumnoMateria = bdRecuperada.keyAlumnoMaterias;
-    controladorBD.baseDatos.keyGrupo = bdRecuperada.keyGrupos;
+    controladorBD.baseDatos.keyGrupos = bdRecuperada.keyGrupos;
     controladorBD.baseDatos.tablaAlumnos = new Map(bdRecuperada.tablaAlumnos);
     controladorBD.baseDatos.tablaMaterias = new Map(bdRecuperada.tablaMaterias);
     controladorBD.baseDatos.tablaAlumnoMateria = new Map(bdRecuperada.tablaAlumnoMaterias);
     controladorBD.baseDatos.tablaGrupos = new Map(bdRecuperada.tablaGrupos);
+
+    console.log("-")
 }
 
 document.addEventListener("DOMContentLoaded", e => {
@@ -195,10 +197,13 @@ $("body").addEventListener("click", (e) => {
         modal.hide();
 
     }
+    if (e.target.id == "verListaGrupos") {
+        cargarTablaGrupos(controladorBD.baseDatos.tablaGrupos);
+    }
     if (e.target.id == "btnAsignarGrupo") {
         let idAlumno = $("#idAlumnoAsignarGrupo").value;
         let idGrupo = $("#selectIdGrupo").value;
-        controladorBD.agregarAlumnoGrupo( idGrupo,idAlumno);
+        controladorBD.agregarAlumnoGrupo(idGrupo, idAlumno);
         let modal = bootstrap.Modal.getInstance("#modalAsignarGrupo");
         modal.hide();
         cargarTablaAlumnos2(controladorBD.baseDatos.tablaAlumnos);
@@ -356,7 +361,7 @@ function cargarTablaAlumnos2(alumnos) {
     tablaAlumno.forEach((alumno) => {
         //debugger
         let grupo = controladorBD.buscarAlumnoEnGrupos(alumno.id);
-       
+
         let botonAsignarGrupo = `<button type="button" class="btn btn-success" title="Asignar grupo" onClick=asignarGrupo(${alumno.id})><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
 </svg></button>`
@@ -393,4 +398,8 @@ function cargarTablaAlumnos2(alumnos) {
     tablaAlumnohtml += `</tbody></table>`;
     //console.log(tablaAlumnohtml)
     tablaContenedor.innerHTML = tablaAlumnohtml;
+}
+function eliminarGrupo(idGrupo){
+    controladorBD.eliminarGrupo(idGrupo);
+    cargarTablaGrupos(controladorBD.baseDatos.tablaGrupos);
 }
